30f8beecb9d7b6336b4c3244e73df6f56e452db1
Brian Lonsdorf <brian@looprecur.com>
2017-01-26 16:30:35 -0800

    readme and nicer colors

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# portion-control
-Badge to quickly communicate project values
+# Portion Control
+
+Render a badge to quickly communicate project values
+
+
+# Usage
+
+  * Meditate on your projects' values. You can pick up to 4.
+
+  * Add an image tag:
+
+    ```
+      <img src="http://portioncontrol.herokuapp.com/pie.png?size=500x500&performance=70&readabilty=25&maintainability=5" alt="performance=70, readabilty=25, maintainability=5" width=250 height=250 />
+    ```
+
+Don't forget the `alt` tag for a11y. Doubling the size makes it look nice on retina screens.
+
+Enjoy!
--- chart-ops.js
+++ chart-ops.js
@@ -2,28 +2,23 @@ module.exports = (width, labels, data) =>
 ({
   type: "doughnut",
   data: {
-    labels: labels,
+    labels,
     datasets: [{
-        data: data,
+        data,
         backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)"
-        ],
-        borderColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)"
-        ],
+          "rgba(255, 105, 120, 1)",
+          "rgba(52, 0, 104, 1)",
+          "rgba(177, 237, 232, 1)",
+          "rgba(109, 67, 90, 1)"
+        ]
     }]
   },
   options:  {
-    legend: {labels: {boxWidth: (width / labels.length) / 3}}
+    legend: {
+      labels: {
+        boxWidth: (width / labels.length) / 4,
+        fontSize: 24
+      }
+    }
   }
 })
